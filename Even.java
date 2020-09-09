8ff99e7d0548b3c02f03c78d90281c107a874b39
souravrawat1112 <66464446+souravrawat1112@users.noreply.github.com>
2020-09-09 13:19:43 +0530

    Add files via upload

---
+++ Even.java
@@ -0,0 +1,11 @@
+class Even {
+    public static void main(String args[]) {
+        int n = 100;
+        System.out.print("Even Numbers from 1 to "+n+" : ");
+        for (int i = 1; i <= n; i++) {
+                        if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
